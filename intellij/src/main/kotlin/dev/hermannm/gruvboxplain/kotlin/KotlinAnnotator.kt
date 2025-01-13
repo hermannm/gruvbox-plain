6a0966eb406ea293c339ee09c4c510df6e36a46f
hermannm <hermann.morkrid@gmail.com>
2025-01-13 07:31:05 +0100

    Fix Kotlin syntax highlighter

--- intellij/src/main/kotlin/dev/hermannm/gruvboxplain/kotlin/KotlinAnnotator.kt
+++ intellij/src/main/kotlin/dev/hermannm/gruvboxplain/kotlin/KotlinAnnotator.kt
@@ -2,43 +2,55 @@ package dev.hermannm.gruvboxplain.kotlin
 
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
+import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.childLeafs
+import com.intellij.psi.util.elementType
 import dev.hermannm.gruvboxplain.highlight
 import dev.hermannm.gruvboxplain.isGenericBracket
 import dev.hermannm.gruvboxplain.keywordHighlighting
 import dev.hermannm.gruvboxplain.punctuationHighlighting
 import dev.hermannm.gruvboxplain.typeHighlighting
 import dev.hermannm.gruvboxplain.valueHighlighting
+import com.intellij.openapi.diagnostic.logger
+
+private val log = logger<KotlinAnnotator>()
 
 class KotlinAnnotator : Annotator {
     override fun annotate(element: PsiElement, annotationHolder: AnnotationHolder) {
+        when (element.elementType.toString()) {
+            // If element is a package/import declaration, we highlight package path elements
+            "PACKAGE_DIRECTIVE",
+            "IMPORT_LIST" -> {
+                element.highlightChildIdentifiers(typeHighlighting, annotationHolder)
+                return
+            }
+        }
+
         val highlighting = when (element.text) {
             "?:", "=" -> keywordHighlighting
             "this", "true", "false", "null" -> valueHighlighting
             "@" -> typeHighlighting
             "<", ">" -> if (element.isGenericBracket()) punctuationHighlighting else return
             "." -> return
-            else -> if (element.isPackagePathElement()) typeHighlighting else return
+            else -> return
         }
 
         element.highlight(highlighting, annotationHolder)
     }
 }
 
-fun PsiElement.isPackagePathElement(): Boolean {
-    if (this.children.isNotEmpty()) {
-        return false
-    }
-
-    // Skips the immediate parent of the original element, as the package/import element is always
-    // at least 2 levels up.
-    var parentElement = this.parent?.parent ?: return false
-
-    while (true) {
-        when (parentElement.javaClass.simpleName) {
-            "KtPackageDirective", "KtImportDirective" -> return true
+/** Applies the given highlighting to all child leaf elements of type 'IDENTIFIER'. */
+fun PsiElement.highlightChildIdentifiers(
+    highlighting: TextAttributesKey,
+    annotationHolder: AnnotationHolder,
+) {
+    for (childElement in this.childLeafs()) {
+        if (childElement.elementType.toString() == "IDENTIFIER") {
+            log.error("Found identifier '${childElement.text}'")
+            childElement.highlight(highlighting, annotationHolder)
+        } else {
+            log.error("Found non-identifier '${childElement.text}' of type '${childElement.elementType.toString()}'")
         }
-
-        parentElement = parentElement.parent ?: return false
     }
 }
