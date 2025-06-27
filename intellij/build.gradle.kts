699c66cfa4b234ce6cf3411d751f9db294b50cb8
hermannm <hermann.morkrid@gmail.com>
2025-06-27 16:30:16 +0200

    Generate IntelliJ plugin change notes from CHANGELOG.md

--- intellij/build.gradle.kts
+++ intellij/build.gradle.kts
@@ -1,12 +1,14 @@
+import org.jetbrains.changelog.Changelog
 import org.jetbrains.intellij.platform.gradle.tasks.RunIdeTask
 
 group = "dev.hermannm"
 
-version = "0.3.1"
+version = "0.3.0"
 
 plugins {
   kotlin("jvm") version "2.2.0"
   id("org.jetbrains.intellij.platform") version "2.6.0"
+  id("org.jetbrains.changelog") version "2.2.1"
 }
 
 repositories {
@@ -27,6 +29,34 @@ intellijPlatform {
     name = "gruvbox-plain"
     ideaVersion { sinceBuild = "251" }
   }
+}
+
+tasks {
+  patchPluginXml {
+    changeNotes.set(
+        provider {
+          buildString {
+            for ((version, changelogItem) in changelog.getAll()) {
+              if (version != "Unreleased") {
+                append("<h3>v${version}</h3>\n")
+                append(
+                    changelog.renderItem(
+                        changelogItem.withHeader(false),
+                        Changelog.OutputType.HTML,
+                    ),
+                )
+              }
+            }
+          }
+        },
+    )
+  }
+}
+
+// Generates IntelliJ plugin change notes from the root CHANGELOG.md
+changelog {
+  path.set(file("../CHANGELOG.md").canonicalPath)
+  headerParserRegex.set(Regex("""v(\d+\.\d+\.\d+)"""))
 }
 
 tasks.named<RunIdeTask>("runIde") {
